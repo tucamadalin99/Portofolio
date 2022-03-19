00036356a0f5afdaf1754a65a9c8559b1177b02a
TucaMadalin <maad368@gmail.com>
2022-03-19 19:45:01 +0200

    Added Parallax cover

--- dev-portofolio/src/pages/about/About.js
+++ dev-portofolio/src/pages/about/About.js
@@ -1,3 +1,11 @@
+import "./About.scss";
+
 export default function AboutPage() {
-    return <div id="about" style={{height: 500}}>About Page</div>
+    return <div id="about">
+        <div className="cmp-about">
+            <div className="cmp-about__content">
+                <h1 className="cmp-about__content-title">Hi, my name is Madalin</h1>
+            </div>
+        </div>
+    </div>
 }
